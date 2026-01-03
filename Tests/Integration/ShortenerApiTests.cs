dbff6cf870eab552dcbc506a43d0ad443c2e8330
Oleg Pona <olegpona606@gmail.com>
2026-01-03 14:00:06 +0200

    add integration test

--- Tests/Integration/ShortenerApiTests.cs
+++ Tests/Integration/ShortenerApiTests.cs
@@ -1,6 +1,51 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using UrlShortener.Core.DTOs;
+using Xunit;
+
 namespace UrlShortener.Tests.Integration;
 
-public class ShortenerApiTests
+public class ShortenerApiTests: IClassFixture<TestAppFactory>
 {
+    private readonly HttpClient _client;
     
+    public ShortenerApiTests(TestAppFactory factory)
+    {
+        _client = factory.CreateClient(new()
+        {
+            AllowAutoRedirect = false
+        });
+    }
+
+    // [Fact]
+    // public async Task Shorten_ThenRedirect_ThenStats_ClicksIncremented()
+    // {
+    //     // Arrange
+    //     var originalUrl = "https://example.com";
+    //     var request = new ShortenRequest(originalUrl);
+    //     
+    //     // Act 1 POST /shorten
+    //     var postResp = await _client.PostAsJsonAsync("/api/Shortener/shorten", request);
+    //     postResp.StatusCode.Should().Be(HttpStatusCode.OK);
+    //     
+    //     var shorten = await postResp.Content.ReadFromJsonAsync<ShortenResult>();
+    //     shorten.Should().NotBeNull();
+    //     shorten!.Code.Should().NotBeNullOrWhiteSpace();
+    //     
+    //     // Act 2: GET redirect
+    //     var redirectResp = await _client.GetAsync($"/api/Shortener/shorten/{shorten.Code}");
+    //     redirectResp.StatusCode.Should().Be(HttpStatusCode.Found);
+    //     redirectResp.Headers.Location!.ToString().Should().Be(originalUrl);
+    //
+    //     // Act 3: GET stats
+    //     var statsResp = await _client.GetAsync($"/api/Shortener/shorten/stats/{shorten.Code}");
+    //     statsResp.StatusCode.Should().Be(HttpStatusCode.OK);
+    //
+    //     var stats = await statsResp.Content.ReadFromJsonAsync<UrlStatsDto>();
+    //     stats.Should().NotBeNull();
+    //     stats!.Code.Should().Be(shorten.Code);
+    //     stats.OriginalUrl.Should().Be(originalUrl);
+    //     stats.Clicks.Should().Be(1);
+    // }
 }
--- Tests/Integration/TestAppFactory.cs
+++ Tests/Integration/TestAppFactory.cs
@@ -1,6 +1,40 @@
+using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using UrlShortener.Infrastructure.DbContext;
+
 namespace UrlShortener.Tests.Integration;
 
-public class TestAppFactory
+public class TestAppFactory : WebApplicationFactory<Program>
 {
-    
+    private DbConnection? _connection;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<UrlShortenerDbContext>));
+            if(descriptor != null) services.Remove(descriptor);
+            
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            services.AddDbContext<UrlShortenerDbContext>(opt => opt.UseSqlite(_connection));
+            
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<UrlShortenerDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        _connection?.Dispose();
+    }
 }
